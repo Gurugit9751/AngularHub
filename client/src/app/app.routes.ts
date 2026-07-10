5722e19e4b6fac64abc14c1524e1f06e30efe551
KumaraGuru <gurusoftrvp@gmail.com>
2026-07-10 18:16:36 +0530

    Login Page section

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,19 +1,31 @@
-
 import { Routes } from '@angular/router';
 
-import { Login } from './features/auth/login/login';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [
+export const appRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home.component').then((m) => m.HomeComponent),
+  },
 
-    {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-    },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+  },
 
-    {
-        path: 'login',
-        component: Login
-    }
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register').then((m) => m.Register),
+  },
 
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
+  },
+
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
--- client/src/app/core/interceptors/auth-interceptor.ts
+++ client/src/app/core/interceptors/auth-interceptor.ts
@@ -1,5 +1,40 @@
+
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { catchError, throwError } from 'rxjs';
+
+import { TokenService } from '../services/token.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+
+  const tokenService = inject(TokenService);
+  const router = inject(Router);
+
+  const token = tokenService.getToken();
+
+  const authRequest = token
+    ? req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+    : req;
+
+  return next(authRequest).pipe(
+    catchError(error => {
+
+      if (error.status === 401) {
+
+        tokenService.removeToken();
+
+        localStorage.removeItem('user');
+
+        router.navigate(['/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
