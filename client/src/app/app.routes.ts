3ecab4f38074883d497dd95f04b2187d0c370635
KumaraGuru <gurusoftrvp@gmail.com>
2026-07-10 19:40:44 +0530

    Login Page section

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,31 +1,16 @@
 import { Routes } from '@angular/router';
-
-import { authGuard } from './core/guards/auth-guard';
-
 export const appRoutes: Routes = [
   {
+    path: 'auth',
+
+    loadChildren: () => import('./features/auth/auth.routes').then((r) => r.AUTH_ROUTES),
+  },
+
+  {
     path: '',
-    loadComponent: () => import('./features/home/home.component').then((m) => m.HomeComponent),
-  },
 
-  {
-    path: 'login',
-    loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
-  },
+    redirectTo: 'auth/login',
 
-  {
-    path: 'register',
-    loadComponent: () => import('./features/auth/register/register').then((m) => m.Register),
-  },
-
-  {
-    path: 'dashboard',
-    canActivate: [authGuard],
-    loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
-  },
-
-  {
-    path: '**',
-    redirectTo: '',
+    pathMatch: 'full',
   },
 ];
